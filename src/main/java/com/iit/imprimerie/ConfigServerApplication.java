7052158dadb6e57f40bbcaf1f12cb0e101e01674
masterkais <kais2032@gmail.com>
2021-12-27 05:21:08 +0100

    first commit for the edge micro-service config-server

---
+++ src/main/java/com/iit/imprimerie/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.iit.imprimerie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
