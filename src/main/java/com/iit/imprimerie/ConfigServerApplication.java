85f3ceed9cae45a3d208b9030091ab59deea138f
masterkais <kais2032@gmail.com>
2022-01-11 19:29:19 +0100

    initialiser le nom du jar

--- src/main/java/com/iit/imprimerie/ConfigServerApplication.java
+++ src/main/java/com/iit/imprimerie/ConfigServerApplication.java
@@ -5,13 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
+@RestController
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
-
+	@GetMapping("/")
+	public String getMessage() {
+		return "Hello in config server project";
+	}
 }
